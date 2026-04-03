58bfad04bc37fb7e27a3b4dcfd2dd186a01b1757
Scott Rigby <scott@r6by.com>
2026-04-03 18:18:13 +0200

    fix(claudeman): restore portable shebangs on container-bound scripts
    Signed-off-by: Scott Rigby <scott@r6by.com>

--- Formula/claudeman.rb
+++ Formula/claudeman.rb
@@ -19,6 +19,13 @@ class Claudeman < Formula
       system "npm", "install", "--production", "--ignore-scripts"
     end
 
+    # Restore portable shebangs on scripts that get copied into containers.
+    # Homebrew rewrites #!/usr/bin/env node to the Homebrew node path, but
+    # these scripts run inside Linux containers where that path doesn't exist.
+    [libexec/"lib/notify.js", libexec/"lib/browser-open.js"].each do |f|
+      inreplace f, %r{^#!.*node.*$}, "#!/usr/bin/env node"
+    end
+
     # Create wrapper script that runs from libexec with node on PATH
     (bin/"claudeman").write_env_script libexec/"claudeman",
       PATH: "#{Formula["node"].opt_bin}:$PATH"
